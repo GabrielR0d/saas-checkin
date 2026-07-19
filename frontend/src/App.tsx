c6a13a08968fe53dd748c2f7caada41568561a1b
Gabriel <gabriel.00rod@gmail.com>
2026-07-19 02:52:50 +0100

    feat: hide cards UI (backend kept for future use)
    - Remove Cartoes from sidebar nav and App router
    - ClientDetailPage: remove cards tab and cards query; show history directly
    - Remove Cartao column from access log history table (always - for WA check-ins)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import { PrivateRoute } from './components/PrivateRoute'
+import { LoginPage } from './pages/LoginPage'
+import { SignupPage } from './pages/SignupPage'
+import { ForgotPasswordPage } from './pages/ForgotPasswordPage'
+import { ResetPasswordPage } from './pages/ResetPasswordPage'
+import { DashboardPage } from './pages/DashboardPage'
+import { ClientsPage } from './pages/ClientsPage'
+import { ClientDetailPage } from './pages/clients/ClientDetailPage'
+import { DevicesPage } from './pages/DevicesPage'
+import { LogsPage } from './pages/LogsPage'
+import { SettingsPage } from './pages/SettingsPage'
+import { BillingPage } from './pages/BillingPage'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route element={<Layout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/clients" element={<ClientsPage />} />
+          <Route path="/clients/:id" element={<ClientDetailPage />} />
+          <Route path="/devices" element={<DevicesPage />} />
+          <Route path="/logs" element={<LogsPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/billing" element={<BillingPage />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
+}
